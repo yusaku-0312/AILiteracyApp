0a314bf8531e2c1b2ac7faacf0247e743314e795
yusaku0312 <yusaku.sv0312@gmail.com>
2024-12-21 16:36:05 +0900

    First Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/company")
+def company():
+    return render_template("company.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
